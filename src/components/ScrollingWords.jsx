7f20c2e06126e0c92a027a459263c7059c06889a
Emilly Budri <emillybudribognar@gmail.com>
2025-03-04 21:52:43 -0300

    bug scrolling words

--- src/components/ScrollingWords.jsx
+++ src/components/ScrollingWords.jsx
@@ -36,7 +36,7 @@ const ScrollingWords = ({ language }) => {
     <div className="relative h-12 w-full bg-gradient-to-r from-purple-900 via-purple-700 to-blue-800 overflow-hidden flex items-center font-bold">
       <div className="relative flex overflow-x-hidden w-full">
         {/* Primeira linha de rolagem */}
-        <div className="py-12 animate-marquee whitespace-nowrap">
+        <div className="py-12 animate-marquee whitespace-nowrap left-0">
           <span className="text-2xl mx-4">{creativity}</span>
           <span className="text-2xl mx-4">•</span>
           <span className="text-2xl mx-4">{innovation}</span>
@@ -50,7 +50,7 @@ const ScrollingWords = ({ language }) => {
         </div>
 
         {/* Segunda linha de rolagem (inicia após a primeira) */}
-        <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
+        <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap left-0">
           <span className="text-2xl mx-4">{creativity}</span>
           <span className="text-2xl mx-4">•</span>
           <span className="text-2xl mx-4">{innovation}</span>
