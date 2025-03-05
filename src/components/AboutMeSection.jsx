b1cd1a3cb7849816f1d8953821c0fa38d90b18f1
Emilly Budri <emillybudribognar@gmail.com>
2025-03-05 05:00:49 -0300

    espaçamento

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -22,6 +22,7 @@ const ProfileCard = ({ language }) => {
         "Sou apaixonada por <b>tecnologia</b> e <b>UX Design</b>, onde <b>psicologia</b>, <b>arte</b> e <b>experiência do usuário</b> se encontram. Acredito que o <b>design</b> deve ser mais do que funcional – ele precisa criar uma <b>experiência intuitiva</b> e alinhada às <b>demandas reais</b> das pessoas.",
       description2:
         "Meu objetivo é desenvolver <b>soluções digitais</b> que fazem a diferença no dia a dia, sempre com <b>comunicação clara</b> e foco nas <b>necessidades reais</b> dos usuários. Sou curiosa e estou sempre explorando <b>novas perspectivas</b> para aprimorar cada detalhe do design.",
+      description3: "Seria um prazer ajudar sua empresa a crescer com soluções digitais que realmente fazem a diferença.",
       buttonText: "BAIXAR CV",
       cvLink: "https://drive.google.com/file/", // Link para o CV em português
     },
@@ -32,6 +33,7 @@ const ProfileCard = ({ language }) => {
         "Me apasiona la <b>tecnología</b> y el <b>Diseño UX</b>, donde se encuentran la <b>psicología</b>, el <b>arte</b> y la <b>experiencia del usuario</b>. Creo que el <b>diseño</b> debe ser más que funcional: debe crear una <b>experiencia intuitiva</b> y alineada con las <b>necesidades reales</b> de las personas.",
       description2:
         "Mi objetivo es desarrollar <b>soluciones digitales</b> que marquen la diferencia en el día a día, siempre con <b>comunicación clara</b> y enfoque en las <b>necesidades reales</b> de los usuarios. Soy curiosa y siempre estoy explorando <b>nuevas perspectivas</b> para mejorar cada detalle del diseño.",
+      description3: "Sería un placer ayudar a su empresa a crecer con soluciones digitales que realmente marcan la diferencia.",
       buttonText: "DESCARGAR CV",
       cvLink: "https://drive.google.com/file/", // Link para o CV em espanhol
     },
@@ -42,6 +44,7 @@ const ProfileCard = ({ language }) => {
         "I am passionate about <b>technology</b> and <b>UX Design</b>, where <b>psychology</b>, <b>art</b>, and <b>user experience</b> come together. I believe that <b>design</b> should be more than functional – it needs to create an <b>intuitive experience</b> aligned with the <b>real demands</b> of people.",
       description2:
         "My goal is to develop <b>digital solutions</b> that make a difference in everyday life, always with <b>clear communication</b> and a focus on the <b>real needs</b> of users. I am curious and always exploring <b>new perspectives</b> to enhance every detail of design.",
+      description3: "It would be a pleasure to help your company grow with digital solutions that truly make a difference.",
       buttonText: "DOWNLOAD CV",
       cvLink: "https://drive.google.com/file/", // Link para o CV em inglês
     },
@@ -53,6 +56,7 @@ const ProfileCard = ({ language }) => {
     roles: translatedRoles,
     description1,
     description2,
+    description3,
     buttonText,
     cvLink,
   } = translations[language];
@@ -119,14 +123,14 @@ const ProfileCard = ({ language }) => {
           transition={{ duration: 0.7, delay: 0.9 }} // Delay maior para aparecer após o subtítulo
           className="flex flex-col items-center w-full md:w-1/3 mb-6 sm:mb-0"
         >
-          <div className="relative w-64 h-64 sm:w-80 sm:h-80 -mt-12 sm:-mt-16 mb-6 sm:mb-8">
+          <div className="relative w-64 h-64 sm:w-80 sm:h-80 sm:-mt-16 mb-6 sm:mb-8">
             <Image
               src={Emilly}
               alt="Emilly Budri"
               className="w-full h-full rounded-full border-4 border-purple-600 sm:mt-2 dark:border-purple-400 shadow-xl transform hover:scale-105 transition-all duration-300"
             />
           </div>
-          <div className="flex gap-6 justify-center items-center">
+          <div className="flex gap-6 lg:mb-10 justify-center items-center">
             <SocialIcons />
           </div>
         </motion.div>
@@ -143,13 +147,17 @@ const ProfileCard = ({ language }) => {
             dangerouslySetInnerHTML={{ __html: description1 }}
           />
           <motion.p
-            className="mx-auto mb-8 sm:mb-10 text-l md:mx-0 text-justify leading-relaxed sm:mt-0 text-gray-700 dark:text-gray-200"
+            className="mx-auto lg:mb-6 sm:mb-10 text-l md:mx-0 text-justify leading-relaxed sm:mt-0 text-gray-700 dark:text-gray-200"
             dangerouslySetInnerHTML={{ __html: description2 }}
+          />
+          <motion.p
+            className="mx-auto sm:mb-12 sm:mt-2 text-l md:mx-0 lg:text-justify sm:text-center leading-relaxed text-gray-700 dark:text-gray-200"
+            dangerouslySetInnerHTML={{ __html: description3 }}
           />
 
           {/* Botão animado */}
           <motion.div
-            className="flex justify-center items-center mt-4 pb-8 sm:pb-12"
+            className="flex justify-center lg:mt-6 items-center mt-0 pb-8 sm:pb-12"
             initial={{ opacity: 0, y: 20 }}
             animate={sectionInView ? { opacity: 1, y: 0 } : {}}
             transition={{ duration: 0.7, delay: 1.7 }} // Delay maior para aparecer após o texto
