86806b9651fc6e8bae091569bf0e98a0d9128281
Emilly Budri <emillybudribognar@gmail.com>
2025-04-14 16:39:28 -0300

    Update ScrollingWords.jsx

--- src/components/ScrollingWords.jsx
+++ src/components/ScrollingWords.jsx
@@ -20,8 +20,8 @@ const ScrollingWords = ({ language }) => {
         <div className="py-12 animate-marquee whitespace-nowrap flex">
           {words.map((word, index) => (
             <React.Fragment key={index}>
-              <span className="text-2xl mx-8">{word}</span>
-              <span className="text-2xl mx-8">•</span>
+              <span className="text-2xl mx-8 text-white">{word}</span>
+              <span className="text-2xl mx-8 text-white">•</span>
             </React.Fragment>
           ))}
         </div>
@@ -30,8 +30,8 @@ const ScrollingWords = ({ language }) => {
         <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap flex">
           {words.map((word, index) => (
             <React.Fragment key={index}>
-              <span className="text-2xl mx-8">{word}</span>
-              <span className="text-2xl mx-8">•</span>
+              <span className="text-2xl mx-8 text-white">{word}</span>
+              <span className="text-2xl mx-8 text-white">•</span>
             </React.Fragment>
           ))}
         </div>
