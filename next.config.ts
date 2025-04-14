1c6abe3a0764e9dbcdad19a611d3ad21e3944274
Emilly Budri <emillybudribognar@gmail.com>
2025-04-14 11:07:35 -0300

    seo

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig = {
   output: 'export', // necessário para gerar arquivos estáticos com next export
   basePath: isProd ? `/${repo}` : '',
   assetPrefix: isProd ? `/${repo}/` : '',
+  images: {
+    unoptimized: true, // Desabilita a otimização de imagens para exportação estática
+  },
 };
 
 export default nextConfig;
