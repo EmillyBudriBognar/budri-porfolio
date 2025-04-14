735261408c28ab99c1c12e61fa0fbaa336b5dd09
Emilly Budri <emillybudribognar@gmail.com>
2025-04-14 12:18:41 -0300

    next

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
-const repo = 'budri-portfolio'; // Nome do repositório GitHub
-
 const nextConfig = {
-  output: 'export', // necessário para gerar arquivos estáticos com next export
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
+  output: 'export',
   images: {
-    unoptimized: true, // Desabilita a otimização de imagens para exportação estática
+    unoptimized: true,
   },
 };
 
